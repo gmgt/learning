7c57d8850c21d0b0fb520c8b629e61388902031d
gmgt <nadine199721@yandex.ru>
2015-09-27 02:36:45 +0400

    in der arbeit

--- ch3.py
+++ ch3.py
@@ -18,16 +18,23 @@
 
 #input("enter")
 
-import random
-n = random.randint(1,5)
-if n == 1:
-    print("a")
-elif n == 2:
-    print("b")
-elif n == 3:
-    print("c")
-elif n == 4:
-    print("d")
-elif n == 5:
-    print("e")
-input("enter")
+#import random
+#n = random.randint(1,5)
+#if n == 1:
+#    print("a")
+#elif n == 2:
+#    print("b")
+#elif n == 3:
+#    print("c")
+#elif n == 4:
+#    print("d")
+#elif n == 5:
+#    print("e")
+#input("enter")
+
+#response = ""
+#while response != ".":
+#    response = input(",")
+#input("enter")
+
+a = 21
